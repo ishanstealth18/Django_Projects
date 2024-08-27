2b4f54ff2cf5828f74191d1ca0320fad47673f30
ishan <ishanstealth18@yahoo.co.in>
2024-08-26 23:50:25 -0400

    Added register function and login function

--- expense_tracker/expense_tracker_app/views.py
+++ expense_tracker/expense_tracker_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from expense_tracker_app.forms import login_form, register_form
 
@@ -15,8 +17,39 @@ def login(request):
 
 def register_user(request):
     form = register_form.RegisterForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            first_name = form.cleaned_data["register_firstname"]
+            last_name = form.cleaned_data["register_lastname"]
+            email = form.cleaned_data["register_email"]
+            password = form.cleaned_data["register_password"]
+            new_user_record = User.objects.create_user(first_name=first_name, last_name=last_name, email=email,
+                                                       password=password, username=first_name)
+            new_user_record.save()
 
+    form = register_form.RegisterForm
     context = {
         "register_form": form,
     }
     return render(request, "register.html", context)
+
+
+def user_login(request):
+    form = login_form.LoginForm(request.POST)
+
+    if request.method == "POST":
+        if form.is_valid():
+            user_name = form.cleaned_data["login_username"]
+            user_password = form.cleaned_data["login_password"]
+
+            validate_user = authenticate(request, username=user_name, password=user_password)
+            if validate_user is not None:
+                login(request)
+    else:
+        form = login_form.LoginForm
+
+    context = {
+        "login_form": form,
+    }
+    return render(request, "login.html", context)
+
