19712ec8ddb1fb063af63bcf264ff3c406f35733
ishan <ishanstealth18@yahoo.co.in>
2024-09-23 22:36:01 -0400

    Added few more unit test for database

--- expense_tracker/expense_tracker_app/models.py
+++ expense_tracker/expense_tracker_app/models.py
@@ -9,7 +9,7 @@ from django.db import models
 class ExpenseDataModel(models.Model):
     expense_category = models.CharField(max_length=100)
     expense_description = models.CharField(max_length=50, null=True, blank=True)
-    expense_amount = models.CharField(max_length=20)
-    expense_date = models.DateField(max_length=50)
+    expense_amount = models.CharField(max_length=5)
+    expense_date = models.DateField(max_length=10)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
 
--- expense_tracker/expense_tracker_app/tests.py
+++ expense_tracker/expense_tracker_app/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import ExpenseDataModel
 
@@ -8,11 +9,28 @@ from .models import ExpenseDataModel
 class ExpenseModelTest(TestCase):
 
     def setUp(self) -> None:
+        user1 = User.objects.create_user(first_name="name1", last_name="name2", email="name1@gmail.com",
+                                 password="1234", username="name1")
         ExpenseDataModel.objects.create(
             expense_category="dfasfsfadfadfadfadsfadfadfadsfadfadfadsfadfaadsfasdfadfadsfadsfadfadsfadfadfdsafadfsadfadfasfadsfasdfadsfadsf",
-            expense_amount=11.3, expense_date="2024-08-02", expense_description="test_description")
+            expense_amount=11.3, user=user1, expense_date="2024-08-02", expense_description="test_description")
 
     def test_category_input_length(self):
         expense_record = ExpenseDataModel.objects.get(expense_amount="11.3")
         expense_category_length = len(expense_record.expense_category)
-        self.assertGreaterEqual(expense_category_length, 100)
+        self.assertLessEqual(expense_category_length, 100)
+
+    def test_description_input_length(self):
+        expense_record = ExpenseDataModel.objects.get(expense_amount="11.3")
+        expense_category_length = len(expense_record.expense_description)
+        self.assertLessEqual(expense_category_length, 50)
+
+    def test_amount_input_length(self):
+        expense_record = ExpenseDataModel.objects.get(expense_amount="11.3")
+        expense_category_length = len(expense_record.expense_amount)
+        self.assertLessEqual(expense_category_length, 5)
+
+    def test_date_input_length(self):
+        expense_record = ExpenseDataModel.objects.get(expense_amount="11.3")
+        expense_category_length = len(expense_record.expense_date)
+        self.assertLessEqual(expense_category_length, 10)
