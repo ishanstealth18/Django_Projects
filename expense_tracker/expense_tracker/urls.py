ec0919f675b1ef19ea940e216e747e5642df93fe
ishan <ishanstealth18@yahoo.co.in>
2024-09-30 22:53:45 -0400

    Updated existing unit test cases

--- expense_tracker/expense_tracker/urls.py
+++ expense_tracker/expense_tracker/urls.py
@@ -21,7 +21,7 @@ from expense_tracker_app import views
 urlpatterns = [
     #path("expense_tracker_app/", include("expense_tracker_app.urls")),
     path('admin/', admin.site.urls),
-    path('', views.user_login, name="login_page"),
+    path('', views.user_login, name="base_page"),
     path('', views.user_logout, name="logout_page"),
     path('register.html', views.register_user, name="register_page"),
     path('home.html', views.home_page, name="home_page"),
