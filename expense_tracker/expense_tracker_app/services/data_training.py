3e0c793cb6b4cdf154c455fb8ca702db2c48cb72
ishan <ishanstealth18@yahoo.co.in>
2024-12-12 11:20:58 -0500

    Added comments for better understanding

--- expense_tracker/expense_tracker_app/services/data_training.py
+++ expense_tracker/expense_tracker_app/services/data_training.py
@@ -1,25 +1,20 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
-from nltk import SyllableTokenizer
-from sklearn import preprocessing
-from sklearn.metrics.pairwise import cosine_similarity
-# To be used for splitting the dataset into training and test sets
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.preprocessing import OneHotEncoder
-from nltk.tokenize import sent_tokenize, word_tokenize
 import nltk
 import geocoder
 from geopy.geocoders import Nominatim
-
-# count vector
 from sklearn.feature_extraction.text import CountVectorizer
 
+# Read raw data file
 real_estate_raw_data = pd.read_excel("D:/Python_Projects/Django_Projects/expense_tracker/updated_data.xlsx")
 nltk.download('punkt_tab')
 
 
+# Function to prepare data set. Includes splitting of data into Train and Test set, Standardization and One Hot Encoding
 def prepare_dataset():
     pd.set_option('display.max_columns', None)
     # Creating a dataframe which has columns with numeric values and are required. Normalization and standardization can
@@ -28,13 +23,12 @@ def prepare_dataset():
         ["Price($)", "Bedrooms", "Bathrooms", "Size (sqft)", "Visit Counter"]].copy()
 
     print(real_estate_df_copy.describe())
-
     # Divide the data into features (X) and target (Y)
     # Data is converted to a panda’s dataframe
     X = pd.DataFrame(real_estate_df_copy.values)
 
     print(X.head())
-
+    # Split dataset into train and test set
     X_train, X_test = train_test_split(X, test_size=0.2)
 
     # Good practice to keep original dataframes untouched for reusability
@@ -55,7 +49,7 @@ def prepare_dataset():
     # Assign column names as in the real estate copy data
     X_train_norm_df.columns = ["Price($)", "Bedrooms", "Bathrooms", "Size (sqft)", "Visit Counter"]
 
-    #print(X_train_norm_df.describe())
+    # print(X_train_norm_df.describe())
 
     # Standardization process
     X_train_std = X_train.copy()
@@ -73,17 +67,17 @@ def prepare_dataset():
     X_train_std_df = pd.DataFrame(X_train_stan)
     X_train_std_df.columns = ["Price($)", "Bedrooms", "Bathrooms", "Size (sqft)", "Visit Counter"]
 
-    #print(X_train_std_df.describe())
-    #print(X_train_std_df)
+    # print(X_train_std_df.describe())
+    # print(X_train_std_df)
 
     # Convert categorical values to numeric using One Hot Encoder
     categorical_columns = real_estate_raw_data[["Building Type", "Utilities"]].copy()
-    #print("categorical columns")
-    #print(categorical_columns)
+    # print("categorical columns")
+    # print(categorical_columns)
 
     category_df = pd.DataFrame(categorical_columns)
-    #print("category desc")
-    #print(category_df.describe())
+    # print("category desc")
+    # print(category_df.describe())
     selected_column_train, selected_column_test = train_test_split(category_df, test_size=0.2)
 
     selected_column_train_copy = selected_column_train.copy()
@@ -98,26 +92,21 @@ def prepare_dataset():
     one_hot_encode_df = pd.DataFrame(one_hot_encoded_train, columns=encoder.get_feature_names_out(selected_columns))
 
 
+# Convert raw address data to vector
 def convert_address_to_vector():
-    # Convert Address column into vector using Word2Vec
     address_column = real_estate_raw_data[["Address"]].copy()
-
     address_df = pd.DataFrame(address_column)
-    #print("Address desc")
+    # print("Address desc")
     address_df.replace(',', '', regex=True)
-    #print(address_df.info())
+    # print(address_df.info())
 
     address_column_train, address_column_test = train_test_split(address_df, test_size=0.2)
     address_column_train_copy = address_column_train.copy()
     address_column_test_copy = address_column_test.copy()
 
-    #print("address train data")
-    #print(address_column_train_copy)
-
-    selected_address_column = address_column_train_copy.select_dtypes(include=['object']).columns.to_list()
-    #print("selected address column")
-    #print(selected_address_column)
-
+    # print("address train data")
+    # print(address_column_train_copy)
+    # Creating another list with all the address in clean format
     address_list = []
     for val in address_column_train_copy.values.tolist():
         if type(val) is list:
@@ -137,57 +126,54 @@ def convert_address_to_vector():
 # combined_df.to_excel("combined.xlsx")
 
 
+# Function to get current location which will be used for recommendation in case there is no user data history
 def get_current_location():
     # get current location
     g = geocoder.ip('me')
-    current_city = g.city
     coordinates = g.latlng
-    #print(coordinates)
     geolocator = Nominatim(user_agent="expense_tracker_app")
     lat = str(coordinates[0])
     lng = str(coordinates[1])
     location = geolocator.reverse(lat + "," + lng)
     location = str(location)
-    #print(location)
+    # print(location)
     return location
 
 
-# convert city to vector for cosine similarity with item matrix
-def convert_location_to_vector():
+# convert raw location address to vector for cosine similarity with item matrix
+def clean_raw_location_data():
     location = get_current_location()
     location = location.lower()
     location = location.replace(',', '')
     return location
 
 
-#############################################################
-
-
+# Function to convert text list to vector using count vector
 def convert_text2vector(text_lst):
-    #print("text list:", text_lst)
+    # print("text list:", text_lst)
     vectorizer = CountVectorizer(stop_words="english")
     vectorizer.fit(text_lst)
     vector = vectorizer.transform(text_lst)
     vector = vector.toarray()
-    #print("vector: ", vector)
+    # print("vector: ", vector)
     return vector
 
 
+# Function to calculate cosine similarity
 def calculate_similarity(v1, v2):
     cosine = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
     # print(cosine)
     return cosine
 
 
-# text to vector using count vector method
-
+# Combined function to convert text to vector and find cosine similarity
 def find_cos_similarity():
-    input_address = convert_location_to_vector()
+    input_address = clean_raw_location_data()
 
     similarity_list = []
     address_data = convert_address_to_vector()
 
-    #print("Address Data: ", address_data)
+    # print("Address Data: ", address_data)
     for item in address_data:
         item = item.replace(',', '')
         text_list = [input_address, item]
@@ -197,17 +183,17 @@ def find_cos_similarity():
         cos_similarity = calculate_similarity(input_vector_list[0], input_vector_list[1])
         similarity_list.append(cos_similarity)
 
-    #print(similarity_list)
+    # print(similarity_list)
     return similarity_list
 
-
+# Function to store top recommended values
 def call_top_values():
     cos_similar_values_list = find_cos_similarity()
     address_list_to_df = pd.DataFrame(cos_similar_values_list, columns=["Address_Cos_Similarity"])
-    #print(address_list_to_df)
+    # print(address_list_to_df)
 
     address_list_to_df = address_list_to_df.loc[~(address_list_to_df["Address_Cos_Similarity"] == 0)]
-    #print(address_list_to_df)
+    # print(address_list_to_df)
 
     address_list_to_df = address_list_to_df.sort_values(by="Address_Cos_Similarity", ascending=False)
     top_5_values = address_list_to_df.head(5).index.to_list()
